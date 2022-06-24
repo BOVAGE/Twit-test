34fcc61ee3fba3c5bfd003cec588e3cdbcec1f41
bovage <fayemibolu2@gmail.com>
2022-06-23 21:24:40 -1200

    chore: add more test to tweets & users

--- tweets/tests.py
+++ tweets/tests.py
@@ -1,19 +1,45 @@
 from django.test import TestCase
+from rest_framework.test import APIRequestFactory
+from rest_framework import status
+from django.urls import reverse
 from .models import Tweet
 from django.contrib.auth import get_user_model
+from .serializers import TweetSerializer
+from rest_framework.test import force_authenticate
+from .views import TweetListCreateView
 
 User = get_user_model()
 
 class TweetTests(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
-        testuser = User.objects.create_user(username="testuser", password="password123")
-        Tweet.objects.create(creator=testuser, text="Hello world")
+    def setUp(self):
+        self.testuser = User.objects.create_user(username="testuser", password="password123")
+        self.testtweet = Tweet.objects.create(creator=self.testuser, text="Hello world")
 
     def test_tweet_content(self):
-        tweet = Tweet.objects.get(id=1)
-        user = User.objects.get(id=1)
+        tweet = Tweet.objects.get(id=self.testtweet.id)
+        user = User.objects.get(id=self.testuser.id)
         expected_object_name = f'{tweet.text}'
         self.assertEqual(expected_object_name, "Hello world")
         self.assertEqual(tweet.creator, user)
+
+
+class TweetListViewTests(TestCase):
+    """
+        Test for GET all tweets API
+    """
+    def setUp(self):
+        self.factory = APIRequestFactory()
+        self.testuser = User.objects.create_user(username="testuser", password="password123")
+        tweets_to_create = [Tweet(creator=self.testuser, text=f"Hello world{i}") for i in range(5)]
+        tweet_objs = Tweet.objects.bulk_create(tweets_to_create)
+
+    def test_get_all_tweets(self):
+        request = self.factory.get(reverse('tweets:tweet'))
+        view = TweetListCreateView
+        force_authenticate(request, user=self.testuser)
+        response = view(request)
+        tweets = Tweet.objects.all()
+        serializer = TweetSerializer(tweets, many=True, context={'request': request})
+        self.assertEqual(response.data, serializer.data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
--- users/tests.py
+++ users/tests.py
@@ -10,49 +10,46 @@ from rest_framework.test import APITestCase
 User = get_user_model()
 SIGNING_KEY = settings.SIMPLE_JWT['SIGNING_KEY']
 ALGORITHM = settings.SIMPLE_JWT['ALGORITHM']
-# Create your tests here.
+
 class UserModelTests(TestCase):
 
-    @classmethod
-    def setUpTestData(cls):
-        simple_user = User.objects.create_user(username="testuser", email="test@gmail.com",
+    def setUp(self):
+        self.simple_user = User.objects.create_user(username="testuser", email="test@gmail.com",
         password="123456")
 
-        super_user = User.objects.create_superuser(username="superuser", email="supertest@gmail.com",
+        self.super_user = User.objects.create_superuser(username="superuser", email="supertest@gmail.com",
         password="123456")
 
     def test_simple_user(self):
-        simple_user = User.objects.get(id=1)
+        simple_user = User.objects.get(id=self.simple_user.id)
         self.assertEqual(simple_user.is_staff, False)
         self.assertEqual(simple_user.is_active, True)
 
     def test_super_user(self):
-        super_user = User.objects.get(id=2)
+        super_user = User.objects.get(id=self.super_user.id)
         self.assertEqual(super_user.is_staff, True)
         self.assertEqual(super_user.is_active, True)
 
     def test_user_token(self):
-        simple_user = User.objects.get(id=1)
-        self.assertIn("refresh", simple_user.get_tokens().keys())
-        self.assertIn("access", simple_user.get_tokens().keys())
-        access_token = simple_user.get_tokens()['access']
+        self.assertIn("refresh", self.simple_user.get_tokens().keys())
+        self.assertIn("access", self.simple_user.get_tokens().keys())
+        access_token = self.simple_user.get_tokens()['access']
         id_from_token = jwt.decode(access_token, SIGNING_KEY, [ALGORITHM])['user_id']
-        self.assertEqual(simple_user.id, id_from_token)
+        self.assertEqual(self.simple_user.id, id_from_token)
 
 
-# class FollowerViewTests(APITestCase):
+class FollowerViewTests(APITestCase):
 
-#     def test_no_followers(self):
-#         register_url = reverse('users:register')
-#         data = {
-#             'username': 'testuser',
-#             'password': 'password',
-#             'password2': 'password'
-#         }
-#         response = self.client.post(register_url, data)
-#         print(response.status_code)
-#         print(response.data)
-#         # url = reverse('users:follower', args=(1,))
-#         # print(">>>>>",url)
-#         # response = self.client.get(url)
-#         # self.assertEqual(response.status_code, status.HTTP_200_OK)
+    def test_no_followers(self):
+        register_url = reverse('users:register')
+        data = {
+            'username': 'testuser',
+            'password': 'password',
+            'password2': 'password'
+        }
+        response = self.client.post(register_url, data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        user = User.objects.get(username='testuser')
+        url = reverse('users:follower', args=(user.id,))
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
